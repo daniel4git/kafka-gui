85062465ee3d2e12f6396085be19c4f4df559bfe
Kevin Spears <kevin@gladly.com>
2018-08-23 00:55:33 -0700

    Added ability to add topic consumers
    Looks like there may be some race conditions, sometimes I get a nullpointer,
    and I don't know what happens with more than one consumer. But adding a single
    consumer works as long as you wait for camel to register all the kafka topics.

--- src/main/kotlin/ui/LogController.kt
+++ src/main/kotlin/ui/LogController.kt
@@ -1,9 +1,11 @@
 package ui
 
+import javafx.beans.property.SimpleBooleanProperty
+import javafx.collections.FXCollections
+import javafx.collections.ObservableList
 import javafx.fxml.FXML
 import javafx.fxml.Initializable
-import javafx.scene.control.Button
-import javafx.scene.control.TextArea
+import javafx.scene.control.*
 import javafx.scene.input.MouseEvent
 import kafka.GuiEndpoint
 import kafka.TopicListener
@@ -11,28 +13,52 @@ import kotlinx.coroutines.experimental.async
 import org.apache.camel.impl.DefaultCamelContext
 import java.net.URL
 import java.util.*
+import javafx.beans.property.SimpleStringProperty
 
 class LogController : Initializable {
 
     private val camelContext = DefaultCamelContext()
+    var topics: ObservableList<TopicRow> = FXCollections.observableArrayList<TopicRow>()
 
-    @FXML
-    var log: TextArea? = null
-
-    @FXML
-    var button: Button? = null
+    @FXML var log: TextArea? = null
+    @FXML var add: Button? = null
+    @FXML var topic: TextField? = null
+    @FXML var isPattern: CheckBox? = null
+    @FXML var table: TableView<TopicRow>? = null
 
     override fun initialize(location: URL?, resources: ResourceBundle?) {
+        table?.items?.setAll(topics)
         async {
             camelContext.start()
             camelContext.addRoutes(GuiEndpoint(log))
         }
     }
 
-    fun click(event: MouseEvent) {
-        async {
-            val listener = TopicListener(".*search.*", true)
-            camelContext.addRoutes(listener)
+    fun addTopic(event: MouseEvent) {
+        val topic = topic?.text ?: ""
+        val isPattern = isPattern?.isSelected ?: false
+
+        if (topic.isNotEmpty()) {
+            topics.add(TopicRow(topic, isPattern, true))
+            camelContext.addRoutes(TopicListener(topic, isPattern))
         }
+    }
+}
+
+class TopicRow constructor(topic: String, isPattern: Boolean, enabled: Boolean) {
+    private val topic = SimpleStringProperty(topic)
+    private val isPattern = SimpleBooleanProperty(isPattern)
+    private val enabled = SimpleBooleanProperty(enabled)
+
+    fun getTopic(): String {
+        return topic.value
+    }
+
+    fun getIsPattern(): Boolean {
+        return isPattern.value
+    }
+
+    fun getEnabled(): Boolean {
+        return enabled.value
     }
 }
